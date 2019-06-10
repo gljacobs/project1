73500fe9b36d577dbb72be6dbe607528f3830f91
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-10 14:04:13 -0700

    add autocomplete functionality

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+var nationalParks = ["Acadia", "American Samoa", "Arches", "Badlands", "Big Bend", "Biscayne", "Black Canyon of the Gunnison", "Bryce Canyon", "Cabrillo", "Canyonlands", "Capitol Reef", "Carlsbad Caverns", "Channel Islands", "Congaree", "Crater Lake", "Cuyahoga Valley", "Death Valley", "Denali", "Dry Tortugas", "Everglades", "Gates of the Arctic", "Gettysburg National Military Park", "Glacier", "Glacier Bay", "Grand Canyon Grand Teton", "Great Basin", "Great Sand Dunes", "Great Smoky Mountains", "Guadalupe Mountains", "Haleakala", "Hawai’i Volcanoes", "Harpers Ferry", "Hot Springs", "Isle Royale", "Joshua Tree", "Katmai", "Kenai Fjords", "Kings Canyon", "Kobuk Valley", "Lake Clark", "Lassen Volcanic", "Mammoth Cave", "Mesa Verde", "Mount Rainier", "North Cascades", "Organ Pipe Cactus", "Olympic", "Petrified Forest", "Pinnacles", "Redwood", "Rocky Mountain", "Saguaro", "Sequoia", "Shenandoah", "Theodore Roosevelt", "Valley Forge", "Virgin Islands", "Voyageurs", "Wind Cave", "Wrangell–St. Elias", "Yellowstone", "Yosemite", "Zion"]
+
+
+$(document).ready(function () {
+    $('input.autocomplete').autocomplete({ source: nationalParks });
+});
+
+$("#search").keypress((event) => {
+    console.log("hi");
+
+    if (event.keyCode === 13) {
+        console.log("hi");
+
+
+        event.preventDefault();
+        var search = $("#search").val().trim();
+        $("#search").val("");
+        var queryURL = "https://developer.nps.gov/api/v1/parks?q=" + search + "&fields=images&api_key=O4VdhmolNStlPLj2bo2DfPKWks3F8J9xfihpGqTf";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            $("body").append("<img src='" + response.data[0].images[0].url + "' style='width: 400px'>")
+        });
+    }
+})
